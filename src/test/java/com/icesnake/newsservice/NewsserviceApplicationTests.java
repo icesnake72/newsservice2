6512000250f10b1385365173ef223d1c0e4a8fdc
icesnake <icesnake72@gmail.com>
2024-01-29 19:28:02 +0900

    Initial commit

---
+++ src/test/java/com/icesnake/newsservice/NewsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icesnake.newsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
